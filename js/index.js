a2cf3ade4caf534eba975bbd842f0b38a779c724
minazul01 <minajul4010@gmail.com>
2024-12-21 16:57:28 +0600

    complete

--- js/index.js
+++ js/index.js
@@ -11,7 +11,6 @@ document.getElementById('history-tab').addEventListener('click', function(){
         "to-purple-600"
     );
     document.getElementById('expense-form').classList.add("hidden");
-    // document.getElementById('results').classList.remove('hidden');
     document.getElementById('history-section').classList.remove('hidden');
     assistantBtn.classList.remove(
         "text-white",
@@ -42,15 +41,19 @@ document.getElementById('assistant-tab').addEventListener('click', function(){
 
 /* calculator btn */
 document.getElementById('calculate').addEventListener('click', function(){
-    const tottalIncome = document.getElementById('income').value;
-    const softwareExpensive = document.getElementById('software').value;
-    const courseExpensive = document.getElementById('courses').value;
-    const internetExpensive = document.getElementById('internet').value;
+    const tottalIncome = parseFloat(document.getElementById('income').value);
+    const softwareExpensive = parseFloat(document.getElementById('software').value);
+    const courseExpensive = parseFloat(document.getElementById('courses').value);
+    const internetExpensive = parseFloat(document.getElementById('internet').value);
+
     /* condition */
     if(tottalIncome <= 0 || isNaN(tottalIncome)){
         /* Income invalid amount */
         const incomeInvalidAmount = document.getElementById('income-error');
         incomeInvalidAmount.classList.remove("hidden");
+        
+        // document.getElementById('results').classList.add('hidden')
+
         document.getElementById('income').value = '';
     }
     if(softwareExpensive > tottalIncome || isNaN(softwareExpensive)){
@@ -67,5 +70,61 @@ document.getElementById('calculate').addEventListener('click', function(){
     }
 
     /* software account */
+    const tottalExpensive = softwareExpensive + courseExpensive + internetExpensive;
+    const expensiveMinus = tottalIncome - tottalExpensive;
+// console.log(tottalExpensive,expensiveMinus)
+    const tottalExpensiveDoom = document.getElementById('total-expenses');
+    tottalExpensiveDoom.innerText = tottalExpensive;
+
+
+    let tottalBalance = document.getElementById('balance');
+    tottalBalance.innerText = expensiveMinus;
     
-})
+
+
+    document.getElementById('results').classList.remove('hidden');
+
+
+
+
+    /* history section added  */
+    const createDiv = document.createElement('div');
+    createDiv.className = "bg-white p-3 border-2 border-indego-500 space-y-4 rounded-lg";
+    createDiv.innerHTML = `
+<p class="text-xs font-normal text-black"></p>
+<h3 class="text-xs text-black bg-white font-bold">${new Date()}</h3>
+<h3 class="text-xs text-black bg-white font-bold">Tottal expensive: ${tottalExpensive}</h3>
+<h3 class="text-xs text-black bg-white font-bold">Tottal balance: ${expensiveMinus}</h3>
+
+    `
+const added = document.getElementById('history-list');
+added.appendChild(createDiv)
+
+});
+
+  /* saving account */
+  document.getElementById('calculate-savings').addEventListener('click', function(){
+     
+
+
+    const reamingElement = document.getElementById('remaining-balance')
+    const savingElement = Number(document.getElementById('savings').value);
+    const balanceElement = Number(document.getElementById('balance').innerText);
+    const savingAmountElement = document.getElementById('savings-amount');
+    
+
+    const savingAmount = balanceElement * savingElement / 100;
+     
+    savingAmountElement.innerText = savingAmount;
+
+    const tottalReamingBalance = balanceElement - savingAmount;
+
+    reamingElement.innerText = tottalReamingBalance;
+    
+
+
+
+    /* history  */
+
+
+  })
